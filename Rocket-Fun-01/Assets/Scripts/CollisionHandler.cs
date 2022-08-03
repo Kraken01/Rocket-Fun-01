5d53a84d058df1c7938ce4f96d15db8fdd22c767
Kraken01 <maxfury078@gmail.com>
2022-08-03 20:38:09 +0530

    Initial

---
+++ Rocket-Fun-01/Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,91 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [SerializeField] float timeToReload = 0f;
+    [SerializeField] AudioClip crashSound;
+    [SerializeField] AudioClip successSound;
+
+    [SerializeField] ParticleSystem crashParticles;
+    [SerializeField] ParticleSystem succesParticles;
+
+    AudioSource audiosource;
+
+    bool isAlive = false;
+    bool collisionDisabled = false;
+    private void Start()
+    {
+        audiosource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void RespondToDebugKeys()
+    {
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (isAlive || collisionDisabled) { return; }
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Friendly");
+                break;
+            case "Finish":
+                StartLevelCompleteSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+    }
+
+    void StartCrashSequence()
+    {
+        isAlive = true;
+        audiosource.Stop();
+        audiosource.PlayOneShot(crashSound);
+        crashParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", timeToReload);
+    }
+
+    void StartLevelCompleteSequence()
+    {
+        isAlive = true;
+        audiosource.Stop();
+        audiosource.PlayOneShot(successSound);
+        succesParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", timeToReload);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;//returns current index in the build setting
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)//it counts the total no. of scenes in the build setting
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
---
+++ Rocket-Fun-01/Assets/Scripts/Movement.cs
@@ -0,0 +1,110 @@
+using UnityEngine; //monobehaviour class present in unity engine namespace
+using UnityEngine.SceneManagement;
+
+public class Movement : MonoBehaviour //inheriting monobehaviour class
+{
+
+    [SerializeField] float playerThrust = 1f;
+    [SerializeField] float playerRotation = 1f;
+    [SerializeField] AudioClip engineThrust;
+
+    [SerializeField] ParticleSystem mainThrust;
+    //[SerializeField] ParticleSystem leftThrust;
+    //[SerializeField] ParticleSystem rightThrust;
+
+
+    Rigidbody rigidbody; //rigidbody type variable
+    AudioSource audioSource;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); //catching reference of rigid body
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Playerthrust();
+        Playerrotation();
+    }
+
+    void Playerthrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            StartThrusting();
+        }
+        else
+        {
+            StopThrusting();
+        }
+    }
+
+    void Playerrotation()
+    {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            RotateLeft();
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            RotateRight();
+        }
+
+        else
+        {
+            StopRotating();
+        }
+    }
+
+    void StartThrusting()
+    {
+        rigidbody.AddRelativeForce(Vector3.up * playerThrust * Time.deltaTime);//vector3.up is a short hand for(0, 1, 0) 
+        //relative force is used to add force related to the cordinates of the object
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(engineThrust);
+        }
+        if (!mainThrust.isPlaying)
+        {
+            mainThrust.Play();
+        }
+    }
+
+    void StopThrusting()
+    {
+        audioSource.Stop();
+        mainThrust.Stop();
+    }
+
+    void RotateLeft()
+    {
+        ApplyRotation(playerRotation);
+       
+    }
+
+    void RotateRight()
+    {
+        ApplyRotation(-playerRotation);
+       
+    }
+
+    void StopRotating()
+    {
+       // leftThrust.Stop();
+       // rightThrust.Stop();
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        rigidbody.freezeRotation = true; //freezing rotation so we can manually control rotation
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);//vector3.forward(0, 0, 1)
+        rigidbody.freezeRotation = false; //unfreezing rotation so physics can take over
+    }
+
+}
+
